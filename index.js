e589c7020160f6b826562a63498d72c119daac0d
Marwa  Awwad <marwaawwad10@gmail.com>
2021-05-19 00:05:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function divide(a,b){
-  return a/b;
-}
+// function divide(a,b){
+//   return a / b;
+// }
+let divide=2000/100
 let square = (parameter1) => parameter1*parameter1
 let add = (parameter1, parameter2) => parameter1 + parameter2
